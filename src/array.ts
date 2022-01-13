78c2d66fb40d0b15f6a356bfec8e85c38469ae6b
Yuta <y.sugiyama54@gmail.com>
2022-01-13 23:13:44 +0900

    array型

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let twoDimensionalArray: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let mix: (string | number | boolean)[] = [1, false, 'japan'];
