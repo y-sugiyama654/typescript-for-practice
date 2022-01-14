8f29305fb59c6bd010cb7f1c1c2c972068038e20
Yuta <y.sugiyama54@gmail.com>
2022-01-14 21:29:39 +0900

    object型2

--- src/object.ts
+++ src/object.ts
@@ -4,4 +4,4 @@ let profile1: object = { name: 'Yuta' };
 profile1 = { birthYear: 1994 };
 
 let profile2: { name: string; } = { name: 'Yuta' };
-profile2 = { birthYear: 1994 };
+profile2 = { name: '1994' };
