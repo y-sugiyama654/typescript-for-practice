b59ae6780fbd5e8f1a101cb93a273dcc75c90c53
Yuta <y.sugiyama54@gmail.com>
2022-01-17 22:40:59 +0900

    オプショナルなパラメータの定義

---
+++ src/optional-arguments.ts
@@ -0,0 +1,20 @@
+export {};
+
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+    height: number, 
+    weight: number,
+    printable?: boolean
+): number => {
+
+    const bmi: number = weight / (height * height);
+
+    if (printable) {
+        console.log(bmi);
+    }
+
+    return bmi;
+}
+
+bmi(1.73, 70);
+bmi(1.73, 70, true);
+bmi(1.73, 70, false);
