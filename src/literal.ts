340d2be581e920031990ae0ffdf95139bf48b50f
Yuta <y.sugiyama54@gmail.com>
2022-01-17 10:36:15 +0900

    literal型

---
+++ src/literal.ts
@@ -0,0 +1,9 @@
+export {};
+
+let dayOfTheWeek: '月' | '火' | '水' | '木' | '菌' | '土' | '日' = '日';
+dayOfTheWeek = '月';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+
+let TRUE: true = true;
