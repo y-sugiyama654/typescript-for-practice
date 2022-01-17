bfc6da312dc2b1ff31c034819c86060e24b9c97a
Yuta <y.sugiyama54@gmail.com>
2022-01-17 22:00:39 +0900

    enum型

---
+++ src/enum.ts
@@ -0,0 +1,29 @@
+export {};
+
+enum Months {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    Jun,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+Months.January;
+Months.March;
+
+enum COLORS {
+    RED = '#FF0000',
+    WHITE = '#FFFFFF',
+    GREEN = '#009000',
+    BLUE = '#0000FF',
+    BLACK = '#000000'
+}
+
+let green = COLORS.GREEN;
