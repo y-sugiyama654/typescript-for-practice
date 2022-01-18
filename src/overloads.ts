8fac21f170a3d120eba197b6c65e6dc6425e0579
Yuta <y.sugiyama54@gmail.com>
2022-01-18 22:24:37 +0900

    関数のオーバーロード

---
+++ src/overloads.ts
@@ -0,0 +1,17 @@
+export {};
+
+function double(value: number): number;
+function double(value: string): string;
+
+function double(value: any): any {
+
+    if (typeof value === 'number') {
+        return value * 2;
+    } else {
+        return value + value;
+    } 
+    console.log(typeof value)
+}
+
+console.log(double(100));
+console.log(double('Kotlin'));
