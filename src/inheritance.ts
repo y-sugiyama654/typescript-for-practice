ee3322bf7e7492beb2d9e36efac986bd0c7be940
Yuta <y.sugiyama54@gmail.com>
2022-01-23 23:39:02 +0900

    継承の使い方

---
+++ src/inheritance.ts
@@ -0,0 +1,29 @@
+export {};
+
+class Animal {
+    constructor(public name: string) {}
+
+    run(): string {
+        return 'I can run';
+    }
+}
+
+class Lion extends Animal {
+    public speed: number;
+    constructor(name: string, speed: number) {
+        super(name);
+
+        this.speed = speed;
+    }
+
+    run(): string {
+        return `${super.run()} ${this.speed}km/h.`;
+    }
+}
+
+// let animal = new Animal();
+// console.log(animal.run());
+// let lion = new Lion();
+// console.log(lion.run());
+console.log(new Animal('ミッキー').run());
+console.log(new Lion('ライオン', 80).run());
